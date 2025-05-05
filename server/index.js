7be2894e204ab45bf341e6f6433c9b8321151bc5
Carlos Ventura <cjventura229822@yahoo.com>
2025-05-05 17:08:11 -0400

    Fix React build path in server/index.js

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// API routes here...
+app.use(express.json());
+
+// API routes
 app.use('/api/trades', require('./routes/trades'));
 
-// Serve static files from React
-app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Fallback to index.html for React Router
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
